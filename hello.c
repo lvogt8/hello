4d0d0916013d782b9e4b53400f8c41102cb783cb
lvogt8 <landvogt03@gmail.com>
2019-08-29 10:31:25 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Landon Vogt
+ * Date: 2019/11/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Landon Vogt\n");
+
+  return 0;
+}
